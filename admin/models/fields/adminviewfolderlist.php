b18930d6b087269738d0d86b7bd9d08169a01339
Llewellyn van der Merwe <llewellyn@vdm.io>
2016-11-02 02:19:25 +0200

    Urgent announcement option added to the new sermon distributor.

---
+++ admin/models/fields/adminviewfolderlist.php
@@ -0,0 +1,167 @@
+<?php
+/*--------------------------------------------------------------------------------------------------------|  www.vdm.io  |------/
+    __      __       _     _____                 _                                  _     __  __      _   _               _
+    \ \    / /      | |   |  __ \               | |                                | |   |  \/  |    | | | |             | |
+     \ \  / /_ _ ___| |_  | |  | | _____   _____| | ___  _ __  _ __ ___   ___ _ __ | |_  | \  / | ___| |_| |__   ___   __| |
+      \ \/ / _` / __| __| | |  | |/ _ \ \ / / _ \ |/ _ \| '_ \| '_ ` _ \ / _ \ '_ \| __| | |\/| |/ _ \ __| '_ \ / _ \ / _` |
+       \  / (_| \__ \ |_  | |__| |  __/\ V /  __/ | (_) | |_) | | | | | |  __/ | | | |_  | |  | |  __/ |_| | | | (_) | (_| |
+        \/ \__,_|___/\__| |_____/ \___| \_/ \___|_|\___/| .__/|_| |_| |_|\___|_| |_|\__| |_|  |_|\___|\__|_| |_|\___/ \__,_|
+                                                        | |                                                                 
+                                                        |_| 				
+/-------------------------------------------------------------------------------------------------------------------------------/
+
+	@version		1.3.4
+	@build			31st October, 2016
+	@created		22nd October, 2015
+	@package		Sermon Distributor
+	@subpackage		adminviewfolderlist.php
+	@author			Llewellyn van der Merwe <https://www.vdm.io/>	
+	@copyright		Copyright (C) 2015. All Rights Reserved
+	@license		GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html 
+	
+	A sermon distributor that links to Dropbox. 
+                                                             
+/-----------------------------------------------------------------------------------------------------------------------------*/
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// import the list field type
+jimport('joomla.form.helper');
+JFormHelper::loadFieldClass('list');
+
+/**
+ * Adminviewfolderlist Form Field class for the Sermondistributor component
+ */
+class JFormFieldAdminviewfolderlist extends JFormFieldList
+{
+	/**
+	 * The adminviewfolderlist field type.
+	 *
+	 * @var		string
+	 */
+	public $type = 'adminviewfolderlist'; 
+	/**
+	 * Override to add new button
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   3.2
+	 */
+	protected function getInput()
+	{
+		// see if we should add buttons
+		$setButton = $this->getAttribute('button');
+		// get html
+		$html = parent::getInput();
+		// if true set button
+		if ($setButton === 'true')
+		{
+			$button = array();
+			$script = array();
+			$buttonName = $this->getAttribute('name');
+			// get the input from url
+			$jinput = JFactory::getApplication()->input;
+			// get the view name & id
+			$values = $jinput->getArray(array(
+				'id' => 'int',
+				'view' => 'word'
+			));
+			// check if new item
+			$ref = '';
+			$refJ = '';
+			if (!is_null($values['id']) && strlen($values['view']))
+			{
+				// only load referal if not new item.
+				$ref = '&amp;ref=' . $values['view'] . '&amp;refid=' . $values['id'];
+				$refJ = '&ref=' . $values['view'] . '&refid=' . $values['id'];
+			}
+			$user = JFactory::getUser();
+			// only add if user allowed to create 
+			if ($user->authorise('core.create', 'com_sermondistributor'))
+			{
+				// build Create button
+				$buttonNamee = trim($buttonName);
+				$buttonNamee = preg_replace('/_+/', ' ', $buttonNamee);
+				$buttonNamee = preg_replace('/\s+/', ' ', $buttonNamee);
+				$buttonNamee = preg_replace("/[^A-Za-z ]/", '', $buttonNamee);
+				$buttonNamee = ucfirst(strtolower($buttonNamee));
+				$button[] = '<a id="'.$buttonName.'Create" class="btn btn-small btn-success hasTooltip" title="'.JText::sprintf('COM_SERMONDISTRIBUTOR_CREATE_NEW_S', $buttonNamee).'" style="border-radius: 0px 4px 4px 0px; padding: 4px 4px 4px 7px;"
+					href="index.php?option=com_sermondistributor&amp;view=&amp;layout=edit'.$ref.'" >
+					<span class="icon-new icon-white"></span></a>';
+			}
+			// only add if user allowed to edit 
+			if (($buttonName == '' || $buttonName == '')  && $user->authorise('core.edit', 'com_sermondistributor'))
+			{
+				// build edit button
+				$buttonNamee = trim($buttonName);
+				$buttonNamee = preg_replace('/_+/', ' ', $buttonNamee);
+				$buttonNamee = preg_replace('/\s+/', ' ', $buttonNamee);
+				$buttonNamee = preg_replace("/[^A-Za-z ]/", '', $buttonNamee);
+				$buttonNamee = ucfirst(strtolower($buttonNamee));
+				$button[] = '<a id="'.$buttonName.'Edit" class="btn btn-small hasTooltip" title="'.JText::sprintf('COM_SERMONDISTRIBUTOR_EDIT_S', $buttonNamee).'" style="display: none; padding: 4px 4px 4px 7px;" href="#" >
+					<span class="icon-edit"></span></a>';
+				// build script
+				$script[] = "
+					jQuery(document).ready(function() {
+						jQuery('#adminForm').on('change', '#jform_".$buttonName."',function (e) {
+							e.preventDefault();
+							var ".$buttonName."Value = jQuery('#jform_".$buttonName."').val();
+							".$buttonName."Button(".$buttonName."Value);
+						});
+						var ".$buttonName."Value = jQuery('#jform_".$buttonName."').val();
+						".$buttonName."Button(".$buttonName."Value);
+					});
+					function ".$buttonName."Button(value) {
+						if (value > 0) {
+							// hide the create button
+							jQuery('#".$buttonName."Create').hide();
+							// show edit button
+							jQuery('#".$buttonName."Edit').show();
+							var url = 'index.php?option=com_sermondistributor&view=&task=.edit&id='+value+'".$refJ."';
+							jQuery('#".$buttonName."Edit').attr('href', url);
+						} else {
+							// show the create button
+							jQuery('#".$buttonName."Create').show();
+							// hide edit button
+							jQuery('#".$buttonName."Edit').hide();
+						}
+					}";
+			}
+			// check if button was created for  field.
+			if (is_array($button) && count($button) > 0)
+			{
+				// Load the needed script.
+				$document = JFactory::getDocument();
+				$document->addScriptDeclaration(implode(' ',$script));
+				// return the button attached to input field.
+				return '<div class="input-append">' .$html . implode('',$button).'</div>';
+			}
+		}
+		return $html;
+	}
+
+	/**
+	 * Method to get a list of options for a list input.
+	 *
+	 * @return	array		An array of JHtml options.
+	 */
+	public function getOptions()
+	{
+		// get custom folder files
+		$localfolder = JPATH_COMPONENT_ADMINISTRATOR.'/views';
+		// set the default
+		$options = array();
+		// import all needed classes
+		jimport('joomla.filesystem.folder');
+		// now check if there are files in the folder
+		if (JFolder::exists($localfolder) && $folders = JFolder::folders($localfolder))
+		{
+			foreach ($folders as $folder)
+			{
+				$options[] = JHtml::_('select.option', $folder, SermondistributorHelper::safeString($folder, 'W'));
+			}
+		}
+		return $options;
+	}
+}
